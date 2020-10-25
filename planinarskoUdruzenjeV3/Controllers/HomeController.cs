7573660c9c49c216bb70174876bf9e31677c0f1e
Jovana <Jovana@DESKTOP-218DU3L>
2020-10-25 21:34:44 +0100

    Identity, Login,Register..

--- planinarskoUdruzenjeV3/Controllers/HomeController.cs
+++ planinarskoUdruzenjeV3/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using planinarskoUdruzenjeV3.Models;
 
 namespace planinarskoUdruzenjeV3.Controllers
 {
+    //[Authorize] da mora login prvo
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
